1ae48cb7b0bd3e8f20122b6255c71ba6d7c33e52
Render78 <mariojuliantor@hotmail.com>
2024-04-01 13:53:01 -0300

    copy of ProductManager in desafio2

---
+++ desafio2/src/ProductManager.js
@@ -0,0 +1,43 @@
+class ProductManager {
+
+    constructor() {
+        this.products = [];
+    }
+
+    addProduct(title, description, price, thumbnail, code, stock) {
+
+        if (!title || !description || !price || !thumbnail || !code || !stock) {
+            throw new Error('Todos los campos son obligatorios');
+        }
+
+        const validateProduct = this.products.find(p => p.code === code);
+        if (validateProduct) {
+            throw new Error('El código del producto ya existe');
+        }
+
+        const product = {
+            id: this.products.length + 1,
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock
+        };
+        this.products.push(product);
+        return product;
+    }
+
+    getProducts() {
+        return this.products;
+    }
+
+    getProductById(id) {
+        const productFound = this.products.find(p => p.id === id);
+        if (productFound != null) {
+            return productFound;
+        } else {
+            throw new Error("Producto no encontrado");
+        }
+    }
+}
