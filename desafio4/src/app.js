55548f7f3f6d3b7cea82ec3c196732367c89ba05
Render78 <mariojuliantor@hotmail.com>
2024-05-01 21:37:10 -0300

    desafio4 done

--- desafio4/src/app.js
+++ desafio4/src/app.js
@@ -1,16 +1,23 @@
-import express from "express";
-import __dirname from "./util.js";
-import handlebars from "express-handlebars";
-import viewsRouter from "./routes/views.router.js"
-import { Server } from "socket.io";
+import express from 'express';
+import __dirname from './utils.js';
+import handlebars from 'express-handlebars';
+import { Server } from 'socket.io';
+import viewsRouter from './routes/views.router.js';
+import { handleSocketConnection } from './controllers/socketController.js';
 
-const app = express();
-const PORT = 8080;
-const httpServer = app.listen(PORT, () => console.log(`Servidor funcionando en el puerto ${PORT}`));
+const app = express()
+const PORT = 8080
+const httpServer = app.listen(PORT, console.log(`Server running on port ${PORT}`))
+const socketServer = new Server(httpServer)
 
-const socketServer = new Server(httpServer);
-app.engine("handlebars", handlebars.engine());
-app.set("views", __dirname+"/views");
-app.set("view engine", "handlebars");
-app.use(express.static(__dirname+"/public"));
-app.use("/", viewsRouter);
+app.engine('handlebars', handlebars.engine())
+app.set('views', __dirname + '/views')
+app.set('view engine', 'handlebars')
+app.use(express.static(__dirname + '/public'))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/', viewsRouter)
+
+handleSocketConnection(socketServer)
