256586de11fd1af50c179e8ac4bedcb6ee1034b1
Render78 <mariojuliantor@hotmail.com>
2024-07-05 17:36:43 -0300

    modified some lines

--- desafio7/src/app.js
+++ desafio7/src/app.js
@@ -4,8 +4,6 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import usersRouter from './routes/users.router.js'
-import ordersRouter from './routes/orders.router.js'
-import businessRouter from './routes/business.router.js'
 import productsRouter from './routes/product.router.js'
 import cartsRouter from './routes/cart.router.js'
 
@@ -21,8 +19,6 @@ app.use(express.urlencoded({ extended: true }))
 
 //Routes
 app.use('/api/users', usersRouter)
-app.use('/api/business', businessRouter)
-app.use('/api/orders', ordersRouter)
 app.use('/api/products', productsRouter)
 app.use('/api/carts', cartsRouter)
 
